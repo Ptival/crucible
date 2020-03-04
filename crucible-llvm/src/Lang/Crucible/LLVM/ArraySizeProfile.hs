06b7a89efd6800ea32ae75de90d49e16d57cec2b
Samuel Breese <samuel@chame.co>
2020-03-04 14:49:01 -0500

    Refactor the arraySizeProfile execution feature

--- crucible-llvm/src/Lang/Crucible/LLVM/ArraySizeProfile.hs
+++ crucible-llvm/src/Lang/Crucible/LLVM/ArraySizeProfile.hs
@@ -2,28 +2,32 @@
 -- |
 -- Module           : Lang.Crucible.LLVM.ArraySizeProfile
 -- Description      : Execution feature to observe argument buffer sizes
--- Copyright        : (c) Galois, Inc 2019
+-- Copyright        : (c) Galois, Inc 2020
 -- License          : BSD3
--- Maintainer       : Andrei Stefanescu <andrei@galois.com>
+-- Maintainer       : Samuel Breese <sbreese@galois.com>
 -- Stability        : provisional
 ------------------------------------------------------------------------
 
-{-# OPTIONS -Wall -Werror #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE ViewPatterns #-}
-{-# LANGUAGE PatternSynonyms #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE KindSignatures #-}
-{-# LANGUAGE TypeFamilies #-}
+{-# Options -Wall #-}
+{-# Language TemplateHaskell #-}
+{-# Language OverloadedStrings #-}
+{-# Language ViewPatterns #-}
+{-# Language PatternSynonyms #-}
+{-# Language FlexibleContexts #-}
+{-# Language ScopedTypeVariables #-}
+{-# Language DataKinds #-}
+{-# Language KindSignatures #-}
+{-# Language TypeFamilies #-}
 
 module Lang.Crucible.LLVM.ArraySizeProfile
-  ( Profile
-  , arraySizeProfile
-  ) where
+ ( FunProfile(..), funProfileName, funProfileArgs
+ , ArgProfile(..), argProfileSize, argProfileInitialized
+ , arraySizeProfile
+ ) where
 
-import qualified Control.Lens as Lens
+import Control.Lens.TH
+
+import Control.Lens
 
 import Data.Type.Equality
 import Data.Foldable
@@ -35,13 +39,12 @@ import Data.Map (Map)
 import qualified Data.Map as Map
 
 import Data.Parameterized.SymbolRepr
-import Data.Parameterized.Context
+import qualified Data.Parameterized.Context as Ctx
 import Data.Parameterized.TraversableFC
 
 import qualified Lang.Crucible.Backend as C
 import qualified Lang.Crucible.Types as C
 import qualified Lang.Crucible.CFG.Core as C
-import qualified Lang.Crucible.Simulator.BoundedExec as C
 import qualified Lang.Crucible.Simulator.CallFrame as C
 import qualified Lang.Crucible.Simulator.EvalStmt as C
 import qualified Lang.Crucible.Simulator.ExecutionTree as C
@@ -56,7 +59,23 @@ import qualified Lang.Crucible.LLVM.Translation.Monad as C
 
 import qualified What4.Interface as W4
 
-type Profile = (Text, [[Maybe Int]])
+------------------------------------------------------------------------
+-- Profiles
+
+data ArgProfile = ArgProfile
+  { _argProfileSize :: Maybe Int
+  , _argProfileInitialized :: Bool
+  } deriving (Show, Eq, Ord)
+makeLenses ''ArgProfile
+
+data FunProfile = FunProfile
+  { _funProfileName :: Text
+  , _funProfileArgs :: [ArgProfile]
+  } deriving (Show, Eq, Ord)
+makeLenses ''FunProfile
+
+------------------------------------------------------------------------
+-- Learning a profile from an ExecState
 
 ptrStartsAlloc ::
   W4.IsExpr (W4.SymExpr sym) =>
@@ -96,7 +115,7 @@ intrinsicArraySize ::
   Maybe Int
 intrinsicArraySize mem
   (testEquality (knownSymbol :: SymbolRepr "LLVM_pointer") -> Just Refl)
-  (Empty :> C.BVRepr _w) i = ptrArraySize mem i
+  (Ctx.Empty Ctx.:> C.BVRepr _w) i = ptrArraySize mem i
 intrinsicArraySize _ _ _ _ = Nothing
 
 regValueArraySize ::
@@ -116,39 +135,48 @@ regEntryArraySize ::
 regEntryArraySize mem (C.RegEntry t v) = regValueArraySize mem t v
 
 newtype Wrap a (b :: C.CrucibleType) = Wrap { unwrap :: a }
-argArraySizes ::
+argProfiles ::
   W4.IsExprBuilder sym =>
   [G.MemAlloc sym] ->
-  Assignment (C.RegEntry sym) ctx ->
-  [Maybe Int]
-argArraySizes mem as = Vector.toList $ toVector (fmapFC (Wrap . regEntryArraySize mem) as) unwrap
+  Ctx.Assignment (C.RegEntry sym) ctx ->
+  [ArgProfile]
+argProfiles mem as = uncurry ArgProfile <$>
+  ( zip (Vector.toList $ Ctx.toVector (fmapFC (Wrap . regEntryArraySize mem) as) unwrap)
+    $ repeat False
+  )
+
+------------------------------------------------------------------------
+-- Execution feature for learning profiles
+
+updateProfiles ::
+  (C.IsSymInterface sym, C.HasPtrWidth (C.ArchWidth arch)) =>
+  C.LLVMContext arch ->
+  IORef (Map Text [FunProfile]) ->
+  C.ExecState p sym (C.LLVM arch) rtp ->
+  IO ()
+updateProfiles llvm cell state
+  | C.CallState _ (C.CrucibleCall _ frame) sim <- state
+  , C.CallFrame { C._frameCFG = cfg, C._frameRegs = regs } <- frame
+  , Just mem <- C.memImplHeap <$> C.lookupGlobal (C.llvmMemVar llvm)
+                (sim ^. C.stateTree . C.actFrame . C.gpGlobals)
+  = do
+      modifyIORef' cell $ \profs ->
+        let name = Text.pack . show $ C.cfgHandle cfg
+            argProfs = argProfiles (G.memAllocs mem) $ C.regMap regs
+            funProf = FunProfile name argProfs
+        in case Map.lookup name profs of
+             Nothing -> Map.insert name [funProf] profs
+             Just variants
+               | funProf `elem` variants -> profs
+               | otherwise -> Map.insert name (funProf:variants) profs
+  | otherwise = pure ()
 
 arraySizeProfile ::
   (C.IsSymInterface sym, C.HasPtrWidth (C.ArchWidth arch)) =>
   C.LLVMContext arch ->
-  IORef (Map Text [[Maybe Int]]) ->
+  IORef (Map Text [FunProfile]) ->
   IO (C.ExecutionFeature p sym (C.LLVM arch) rtp)
 arraySizeProfile llvm cell = do
-  be <- C.runExecutionFeature . C.genericToExecutionFeature
-        <$> C.boundedExecFeature (const . pure $ Just 0) False
   pure . C.ExecutionFeature $ \s -> do
-    case s of
-      C.CallState _
-        (C.CrucibleCall _
-          C.CallFrame { C._frameCFG = g
-                      , C._frameRegs = regs
-                      }) sim ->
-        let globals = Lens.view (C.stateTree . C.actFrame . C.gpGlobals) sim
-        in case C.memImplHeap <$> C.lookupGlobal (C.llvmMemVar llvm) globals of
-          Nothing -> pure ()
-          Just mem -> do
-            modifyIORef' cell $ \profs ->
-              let name = Text.pack . show $ C.cfgHandle g
-                  sizes = argArraySizes (G.memAllocs mem) $ C.regMap regs
-              in case Map.lookup name profs of
-                Nothing -> Map.insert name [sizes] profs
-                Just variants
-                  | sizes `elem` variants -> profs
-                  | otherwise -> Map.insert name (sizes:variants) profs
-      _ -> pure ()
-    be s
+    updateProfiles llvm cell s
+    pure C.ExecutionFeatureNoChange
