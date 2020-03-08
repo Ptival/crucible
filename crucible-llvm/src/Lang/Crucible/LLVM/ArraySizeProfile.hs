e3e70fa2fcdcc2637b3b62d050391e147d8ea02c
Samuel Breese <samuel@chame.co>
2020-03-08 18:00:01 -0400

    Identify loop starts and a trivial generalize

--- crucible-llvm/src/Lang/Crucible/LLVM/ArraySizeProfile.hs
+++ crucible-llvm/src/Lang/Crucible/LLVM/ArraySizeProfile.hs
@@ -11,13 +11,16 @@
 {-# Options -Wall #-}
 {-# Language TemplateHaskell #-}
 {-# Language OverloadedStrings #-}
+{-# Language LambdaCase #-}
 {-# Language ViewPatterns #-}
 {-# Language PatternSynonyms #-}
+{-# Language BangPatterns #-}
 {-# Language FlexibleContexts #-}
 {-# Language ScopedTypeVariables #-}
 {-# Language DataKinds #-}
 {-# Language KindSignatures #-}
 {-# Language TypeFamilies #-}
+{-# Language GADTs #-}
 
 module Lang.Crucible.LLVM.ArraySizeProfile
  ( FunProfile(..), funProfileName, funProfileArgs
@@ -27,6 +30,9 @@ module Lang.Crucible.LLVM.ArraySizeProfile
 
 import Control.Lens.TH
 
+import System.IO
+
+import Control.Monad
 import Control.Lens
 
 import Data.Type.Equality
@@ -37,20 +43,27 @@ import qualified Data.Text as Text
 import qualified Data.Vector as Vector
 import Data.Map (Map)
 import qualified Data.Map as Map
+import Data.Set (Set)
+import qualified Data.Set as Set
 
+import Data.Parameterized.Some
 import Data.Parameterized.SymbolRepr
 import qualified Data.Parameterized.Context as Ctx
 import Data.Parameterized.TraversableFC
 
+import qualified Lang.Crucible.Analysis.Fixpoint as C
+import qualified Lang.Crucible.Analysis.Fixpoint.Components as C
 import qualified Lang.Crucible.Backend as C
-import qualified Lang.Crucible.Types as C
 import qualified Lang.Crucible.CFG.Core as C
+import qualified Lang.Crucible.CFG.Extension as C
+import qualified Lang.Crucible.FunctionHandle as C
 import qualified Lang.Crucible.Simulator.CallFrame as C
 import qualified Lang.Crucible.Simulator.EvalStmt as C
 import qualified Lang.Crucible.Simulator.ExecutionTree as C
 import qualified Lang.Crucible.Simulator.GlobalState as C
 import qualified Lang.Crucible.Simulator.Intrinsics as C
 import qualified Lang.Crucible.Simulator.RegMap as C
+import qualified Lang.Crucible.Types as C
 
 import qualified Lang.Crucible.LLVM.Extension as C
 import qualified Lang.Crucible.LLVM.MemModel as C
@@ -146,6 +159,58 @@ argProfiles mem as = uncurry ArgProfile <$>
   )
 
 ------------------------------------------------------------------------
+-- Generalize loops
+
+type WTOMap = Map Int (Int, Int)
+
+buildWTOMap :: [C.WTOComponent (Some (C.BlockID blocks))] -> WTOMap
+buildWTOMap = snd . go 0 0 Map.empty
+  where
+    go :: Int -> Int -> WTOMap -> [C.WTOComponent (Some (C.BlockID blocks))] -> (Int, WTOMap)
+    go !x !_ m [] = (x, m)
+    go !x !d m (C.Vertex (Some bid):cs) =
+       let m' = Map.insert (Ctx.indexVal $ C.blockIDIndex bid) (x, d) m
+       in go (x + 1) d m' cs
+    go !x !d m (C.SCC (Some hd) subcs : cs) =
+       let m' = Map.insert (Ctx.indexVal $ C.blockIDIndex hd) (x, d + 1) m
+           (x', m'') = go (x + 1) (d + 1) m' subcs
+       in go x' d m'' cs
+
+isBackedge ::
+  WTOMap ->
+  Some (C.BlockID blocks) ->
+  C.BlockID blocks args ->
+  Bool
+isBackedge wtoMap (Some current) target
+  | Just (cx, _) <- Map.lookup (Ctx.indexVal $ C.blockIDIndex current) wtoMap
+  , Just (tx, _) <- Map.lookup (Ctx.indexVal $ C.blockIDIndex target) wtoMap
+  , tx <= cx
+  = True
+  | otherwise = False
+
+generalizeMemory ::
+  G.Mem sym {- ^ Old memory -} ->
+  G.Mem sym {- ^ New memory -} ->
+  IO (Maybe (G.Mem sym)) {- ^ Nothing if old memory already generalizes new memory -}
+generalizeMemory _old new = do
+  putStrLn "generalizing..."
+  pure $ Just new
+
+debugState :: C.ExecState p sym ext rtp -> String
+debugState C.ResultState{} = "Result"
+debugState C.AbortState{} = "Abort"
+debugState C.UnwindCallState{} = "UnwindCall"
+debugState C.CallState{} = "Call"
+debugState C.TailCallState{} = "TailCall"
+debugState C.ReturnState{} = "Return"
+debugState C.RunningState{} = "Running"
+debugState C.SymbolicBranchState{} = "SymbolicBranch"
+debugState C.ControlTransferState{} = "ControlTransfer"
+debugState C.OverrideState{} = "Override"
+debugState C.BranchMergeState{} = "BranchMerge"
+debugState C.InitialState{} = "InitialState"
+
+------------------------------------------------------------------------
 -- Execution feature for learning profiles
 
 updateProfiles ::
@@ -157,8 +222,7 @@ updateProfiles ::
 updateProfiles llvm cell state
   | C.CallState _ (C.CrucibleCall _ frame) sim <- state
   , C.CallFrame { C._frameCFG = cfg, C._frameRegs = regs } <- frame
-  , Just mem <- C.memImplHeap <$> C.lookupGlobal (C.llvmMemVar llvm)
-                (sim ^. C.stateTree . C.actFrame . C.gpGlobals)
+  , Just mem <- C.memImplHeap <$> C.lookupGlobal (C.llvmMemVar llvm) (sim ^. C.stateGlobals)
   = do
       modifyIORef' cell $ \profs ->
         let name = Text.pack . show $ C.cfgHandle cfg
@@ -172,11 +236,68 @@ updateProfiles llvm cell state
   | otherwise = pure ()
 
 arraySizeProfile ::
+  forall sym arch p rtp.
   (C.IsSymInterface sym, C.HasPtrWidth (C.ArchWidth arch)) =>
   C.LLVMContext arch ->
   IORef (Map Text [FunProfile]) ->
   IO (C.ExecutionFeature p sym (C.LLVM arch) rtp)
-arraySizeProfile llvm cell = do
+arraySizeProfile llvm profiles = do
+  (frameLoopStarts :: IORef (Map Text (Set Int))) <- newIORef Map.empty
+  (frameWTOMaps :: IORef (Map Text WTOMap)) <- newIORef Map.empty
+  (frameMemCache :: IORef (Map Text (G.Mem sym))) <- newIORef Map.empty
   pure . C.ExecutionFeature $ \s -> do
-    updateProfiles llvm cell s
-    pure C.ExecutionFeatureNoChange
+    hPutStrLn stderr $ debugState s
+    updateProfiles llvm profiles s
+    case s of
+      C.CallState _ (C.CrucibleCall _ C.CallFrame { C._frameCFG = g }) _ -> do
+        let name = Text.pack . show $ C.cfgHandle g
+        let wtoMap = buildWTOMap $ C.cfgWeakTopologicalOrdering g
+        modifyIORef frameWTOMaps $ \fwto ->
+          case Map.lookup name fwto of
+            Just _ -> fwto
+            Nothing -> Map.insert name wtoMap fwto
+        pure C.ExecutionFeatureNoChange
+      C.RunningState (C.RunBlockStart bid) st -> do
+        frameStarts <- readIORef frameLoopStarts
+        let name = Text.pack . show . C.frameHandle $ st ^. C.stateCrucibleFrame
+        case Map.lookup name frameStarts of
+          Just starts
+            | Set.member (Ctx.indexVal $ C.blockIDIndex bid) starts
+            , Just memImpl <- C.lookupGlobal (C.llvmMemVar llvm) (st ^. C.stateGlobals)
+              -> do
+                fmc <- readIORef frameMemCache
+                case Map.lookup name fmc of
+                  Just oldMem -> generalizeMemory oldMem (C.memImplHeap memImpl) >>= \case
+                    Just genMem -> do
+                      writeIORef frameMemCache $ Map.insert name genMem fmc
+                      let st' = st & C.stateGlobals
+                            %~ C.insertGlobal (C.llvmMemVar llvm) memImpl { C.memImplHeap = genMem }
+                      pure . C.ExecutionFeatureModifiedState
+                        $ C.RunningState (C.RunBlockStart bid) st'
+                    Nothing ->
+                      -- TODO: quit loop
+                      pure C.ExecutionFeatureNoChange
+                  _ -> do
+                    writeIORef frameMemCache $ Map.insert name (C.memImplHeap memImpl) fmc
+                    pure C.ExecutionFeatureNoChange
+          _ -> pure C.ExecutionFeatureNoChange
+      C.ControlTransferState (C.ContinueResumption (C.ResolvedJump target _)) st ->
+        transition frameLoopStarts frameWTOMaps target st
+      C.ControlTransferState (C.CheckMergeResumption (C.ResolvedJump target _)) st ->
+        transition frameLoopStarts frameWTOMaps target st
+      _ -> pure C.ExecutionFeatureNoChange
+  where
+    transition frameLoopStarts frameWTOMaps target st = do
+      let name = Text.pack . show . C.frameHandle $ st ^. C.stateCrucibleFrame
+      fwto <- readIORef frameWTOMaps
+      case Map.lookup name fwto of
+        Just wtoMap
+          | isBackedge wtoMap (st ^. C.stateCrucibleFrame . C.frameBlockID) target
+            -> modifyIORef frameLoopStarts
+               $ Map.alter
+               ( \case
+                   Just s -> Just $ Set.insert (Ctx.indexVal $ C.blockIDIndex target) s
+                   Nothing -> Just . Set.singleton . Ctx.indexVal $ C.blockIDIndex target
+               ) name
+        _ -> pure ()
+      pure C.ExecutionFeatureNoChange
